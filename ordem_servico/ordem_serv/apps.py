181da96cf163d11c387c94a6a9b4d9d96223e447
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-14 12:43:29 -0200

    Refazendo projeto ordem-servico

--- ordem_servico/ordem_serv/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrdemServConfig(AppConfig):
-    name = 'ordem_serv'
--- ordem_servico/ordem_serv/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import TipoServico
-
-def home(request):
-    lista = {}
-    lista['tipos'] = TipoServico.objects.all()
-    return render(request, 'ordem_serv/home', lista)
