fb327f1ae7b7b2c1c996993d2e386b7c146e5094
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-14 01:21:47 -0200

    Resolvendo bug no migrate, corrigindo campos do TipoServico

--- ordem_servico/ordem_serv/models.py
+++ ordem_servico/ordem_serv/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class TipoServico(models.Model):
-    descricao = models.CharField(max_length=50)
-    dt_criacao = models.DateTimeField(auto_now_add=True)
+    #dt_criacao = models.DateTimeField(auto_now_add=True)
+    tipo = models.CharField(max_length=50, blank=False)
+    descricao = models.TextField(max_length=200, blank=False)
 
     def __str__(self):
-        return self.descricao
+        return self.tipo
 
 
