0931a3dc723ea80971539e298fe7afd06d5661ab
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-15 22:53:16 -0200

    Add projeto cursos_onlien em Dajngo 2.0

---
+++ Cursos_Online/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\diego\desktop\django-framework\cursos_online\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
