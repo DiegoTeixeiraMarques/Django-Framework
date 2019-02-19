e36be1b55200dda5de25bc03fb7f418b4263c0b0
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-19 17:26:08 -0300

    Corrigindo links entre urls com namespace

--- Cursos_Online/cursos_online/cursos_online/courses/views.py
+++ Cursos_Online/cursos_online/cursos_online/courses/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):
+    #template_name = 'courses/index.html'
+    return HttpResponse('Hello')#render(request, template_name)
+
--- Cursos_Online/cursos_online/cursos_online/urls.py
+++ Cursos_Online/cursos_online/cursos_online/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from .core import urls
+from .courses import courses_urls
 
 urlpatterns = [
     path('', include( urls, namespace='core')),                       # Redireciona para as urls dentro da pasta core do projeto
+    path('cursos/', include(courses_urls, namespace='courses')),
     path('admin/', admin.site.urls),
 ]
