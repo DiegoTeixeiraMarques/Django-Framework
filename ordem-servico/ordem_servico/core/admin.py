9a7525cc781da16bd56adde1b8564f7ee9af4a2b
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-20 00:55:59 -0300

    Criando primeiros models

--- ordem-servico/ordem_servico/core/admin.py
+++ ordem-servico/ordem_servico/core/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import TipoServico
+from .models import TipoServico, Setor, Ugb
+
+# ---------------------------------------------------------------- #
 
 class TipoServicoAdmin(admin.ModelAdmin):
-    list_display = ['descricao']                                         # Campos que aparecem na listagem dos objetos no admin
-    search_fields = ['descricao']                                        # Campos pesquisáveis no admin
-
+    list_display = ['descricao']
+    search_fields = ['descricao']
 admin.site.register(TipoServico, TipoServicoAdmin)
+
+# ---------------------------------------------------------------- #
+
+class SetorAdmin(admin.ModelAdmin):
+    list_display = ['centro_custo', 'nome']
+    search_fields = ['nome']
+admin.site.register(Setor, SetorAdmin)
+
+# ---------------------------------------------------------------- #
+
+class UgbAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'nome']
+    search_fields = ['nome']
+admin.site.register(Ugb, UgbAdmin)
+
+# ---------------------------------------------------------------- #
+
--- ordem-servico/ordem_servico/core/models.py
+++ ordem-servico/ordem_servico/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+# ---------------------------------------------------------------- #
+
 class TipoServico(models.Model):
-    descricao = models.CharField(max_length=60)
+    descricao = models.CharField(max_length=60, blank=False)
 
     def __str__(self):
         return self.descricao
@@ -9,3 +11,29 @@ class TipoServico(models.Model):
     class Meta:
         verbose_name = 'Tipo de serviço'
         verbose_name_plural = 'Tipos de serviços'
+
+# ---------------------------------------------------------------- #
+
+class Setor(models.Model):
+    centro_custo = models.IntegerField(unique=True)
+    nome = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Setor'
+        verbose_name_plural = 'Setores'
+
+# ---------------------------------------------------------------- #
+
+class Ugb(models.Model):
+    codigo = models.PositiveIntegerField(unique=True)
+    nome = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'UGB'
+        verbose_name_plural = 'UGBs'
